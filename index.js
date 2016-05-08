ecbf90d3f1c7e8eb8ac0bbc889483754f3e7ea88
changqingniubi <981603572@qq.com>
2016-05-08 10:46:25 +0800

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2016/5/8.
+ */
+console.log("hello");
+var person=require("./person.js");
---
+++ person.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2016/5/8.
+ */
+console.log("person");
